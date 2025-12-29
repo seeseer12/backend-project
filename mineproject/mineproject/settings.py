affc9e8284cfcd6b9f918bff2d1ff271be7f190f
Shishir <bseeseer@gmail.com>
2025-12-29 10:01:10 +0545

    apps added

--- mineproject/mineproject/settings.py
+++ mineproject/mineproject/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+
+]
+
+EXTERNAL_APPS = [
+    'home',
 ]
 
 MIDDLEWARE = [
